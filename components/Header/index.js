b59063f2d973b140dc478495e1ad6935290e0fdd
Rohan Muskawad <84379921+rohan-musk@users.noreply.github.com>
2023-01-29 15:35:30 +0530

    RM|Fix bugs

--- components/Header/index.js
+++ components/Header/index.js
@@ -77,7 +77,7 @@ const Header = ({ handleWorkScroll, handleAboutScroll, isBlog }) => {
                   {showResume && (
                     <Button
                       onClick={() =>
-                        window.open('mailto:hello@chetanverma.com')
+                        window.open('mailto:f20201798@pilani.bits-pilani.ac.in')
                       }
                     >
                       Resume
@@ -85,7 +85,9 @@ const Header = ({ handleWorkScroll, handleAboutScroll, isBlog }) => {
                   )}
 
                   <Button
-                    onClick={() => window.open('mailto:hello@chetanverma.com')}
+                    onClick={() =>
+                      window.open('mailto:f20201798@pilani.bits-pilani.ac.in')
+                    }
                   >
                     Contact
                   </Button>
